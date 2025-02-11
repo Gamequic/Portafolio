9a68520bab36f101e1f044cb8ba3285a448a98b0
Gamequic <demiancalleros1@gmail.com>
2025-02-11 10:34:47 -0700

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages no soporta optimización de imágenes de Next.js
+  },
+  basePath: "/Portafolio", // Reemplaza con el nombre de tu repositorio
+};
 };
 
 export default nextConfig;
